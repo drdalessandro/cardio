d71c38f8588b1a66ec51ca3526f5cac0c10ea73b
drdalessandro <admin@epa-bienestar.com>
2025-11-11 21:46:48 +0000

    first commit

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const MEDPLUM_BASE_URL = 'https://api.epa-bienestar.com.ar';
+export const MEDPLUM_PROJECT_ID = '0197600e-f482-702e-88a9-85ad894acfab';
+export const MEDPLUM_GOOGLE_CLIENT_ID = '472653584585-tbrdnh8jnh7j4gtme7k0k1j70euvlto4.apps.googleusercontent.com';
+export const MEDPLUM_RECAPTCHA_SITE_KEY = '6LfsKpopAAAAACy0Lyc3hYyEjTAEndxrcNGibX9S';
+export const MEDPLUM_REGISTER_ENABLED = 'true';
+export const WDS_SOCKET_PORT = '3000';
